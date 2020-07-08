570519f6718fa242e90df0a0f8f429fd02ddb383
hitozz <41472012+hitozz@users.noreply.github.com>
2020-07-08 15:11:18 +0300

    Strings arrays (#5)
    * Coniditons and cycles homework
    
    * Homework 6
    
    * Homework 5
    
    * Strings-task-update

--- tasks/strings/strings.js
+++ tasks/strings/strings.js
@@ -3,8 +3,19 @@
  * if a number is passed
  * @param {string} str
  */
-function reverseString(str) {
+function reverseString(str){
+  if (typeof str !== "string"){
+    return 'This is not a string!';
+  } else {
+    return str.split("").reverse().join(""); //update to do the code below in one line
+    // str = str.split("");
+    // str = str.reverse();
+    // str = str.join("");
+    // return str;
+  }
 }
+console.log(reverseString("abcde"));
+console.log(reverseString(12345));
 
 /**
  * 1. Calculate a century by given year
@@ -12,8 +23,19 @@ function reverseString(str) {
 * @param {number} year
 */
 function centuryFromYear(year) {
-
+  if (typeof year !== "number") {
+    return 'It is not a number';
+  } 
+  else
+   {
+      return Math.ceil(year/100);}
 }
+console.log(centuryFromYear("qwerty"));
+console.log(centuryFromYear(5));
+console.log(centuryFromYear(100));
+console.log(centuryFromYear(1502));
+console.log(centuryFromYear(2000));
+console.log(centuryFromYear(2020));
 
 /**
  * Calculate count of the provided char in the string
@@ -21,7 +43,12 @@ function centuryFromYear(year) {
  * @param {string} char
  */
 function strCount(str, char) {
-}
+    const regex = new RegExp(char, 'g');
+    const result = str.match(regex);
+    return result ? result.length : 0;
+  }
+  console.log (strCount("makarevich", "a"));
+
 
 /**
  * We need to reduce the length of the string or truncate it if it is longer
@@ -30,7 +57,12 @@ function strCount(str, char) {
  * @param {num} num - by wht amount of chars it should be truncated
  */
 function truncateString(str, num) {
+  if (str.length <= num) {
+    return str;
+  }
+    return str.slice(0, num) + '...';
 }
+truncateString("Good morning", 10);
 
 /**
  * replace 10 with 'ten' word
@@ -40,8 +72,10 @@ function truncateString(str, num) {
  * console.log(replace10("231054")) // 23ten54
  */
 function replace10(text) {
-
+  const regex = /10/g;
+  return text.replace(regex,"ten");
 }
+console.log(replace10 ("8107104"));
 
 /**
  * replace value in square brackets with CONFIDENTIAL
@@ -51,8 +85,10 @@ function replace10(text) {
  * console.log(replaceConfidential("lorem [ipsum] si dolor")) // lorem [CONFIDENTIAL] si dolor
  */
 function replaceConfidential(text) {
-
+  const regex = /\[.*?\]/g;
+  return text.replace (  regex, "[CONFIDENTIAL]")
 }
+console.log(replaceConfidential("lorem [ipsum] si dolor"));
 
 module.exports = {
   reverseString,
