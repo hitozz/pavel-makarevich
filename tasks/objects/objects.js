f9f7bdbe7e7221f6abae7d3ff4c2f7c96e4e1bf0
hitozz <41472012+hitozz@users.noreply.github.com>
2020-07-14 17:11:04 +0300

    Objects - homework 6 (#6)
    * Coniditons and cycles homework
    
    * objects-task

--- tasks/objects/objects.js
+++ tasks/objects/objects.js
@@ -2,18 +2,29 @@
  * create object person with firstName and lastName properties
  * implement method getFullName that returns full name
  */
-const person = //put you object here
-
+const person = { //put you object here
+    firstName: "Pavel",
+    lastName: "Makarevich",
+    getFullName: function (){
+        return `${this.firstName} ${this.lastName}`;
+        }
+    };
 /**
  * create object student that has property grade and method getGrade
  * set person as prototype of student
  */
-const student = //put you object here
+const student = { //put you object here
+    grade: 10,
+    getGrade: function() {
+        return this.grade;
+    }
+};
+Object.setPrototypeOf(student, person);
 
 /**
  * create new instance of student using Object.create
  */
-const student2 = //put you object here
+const student2 = Object.create(student); //put you object here
 
 module.exports = {
     person,
