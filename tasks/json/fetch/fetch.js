8053e57b2ca1d93ddbc24736c0d6afb764e96d4f
hitozz <41472012+hitozz@users.noreply.github.com>
2020-08-06 11:11:04 +0300

    NodeJS-JSON task (#10)
    * NodeJS-JSON task
    
    * fetch using async/await syntax

--- tasks/json/fetch/fetch.js
+++ tasks/json/fetch/fetch.js
@@ -4,4 +4,43 @@
  * 3. Save only those items, where id < 20
  * DOCS: https://www.npmjs.com/package/node-fetch
  */
-const fetch = require('node-fetch');
+
+// const fetch = require('node-fetch');
+
+// const fs = require('fs');
+// const path = require('path');
+// const filePath = path.join(__dirname, 'response.json');
+
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(res => res.json())
+//     .then(data => {
+//         const refined = data.filter(item => item.id < 20);
+//         const stringified = JSON.stringify(refined);
+//         fs.appendFile(filePath, stringified, err => {
+//             if (err) {
+//                 throw err;
+//             }
+//         });
+//     });
+
+const fetch = require("node-fetch");
+
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join(__dirname, "response.json");
+
+async function fetchPostsID() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  const refined = data.filter((item) => item.id < 20);
+  const stringified = JSON.stringify(refined);
+
+  fs.appendFile(filePath, stringified, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+}
+
+fetchPostsID();
